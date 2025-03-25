1585cd9da81fe3969b62551fb50551e0a72138e7
Lucas Martins <lucasromadev@gmail.com>
2025-03-25 17:24:33 -0300

    Exercícios de 1 a 2 JS

---
+++ ExercíciosJS/ex1.js
@@ -0,0 +1,8 @@
+function ParOuImpar() {
+var numero = prompt("Digite um numero");
+
+if( numero % 2 == 0){
+	alert("O número é par");
+}else{
+	alert("O número é ímpar");
+} }
---
+++ ExercíciosJS/ex2.js
@@ -0,0 +1,10 @@
+function npz() {
+    var numero = prompt("Digite um numero");
+
+if( numero == 0){
+	alert("O número é ZERO");
+}else if( numero > 0){
+	alert("O número é POSITIVO");
+}else
+alert("O número é NEGATIVO") }
+
