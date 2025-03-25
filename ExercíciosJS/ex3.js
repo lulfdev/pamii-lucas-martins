8229433f7a61c546e6c60e9acea18ee21decfee3
Lucas Martins <lucasromadev@gmail.com>
2025-03-25 17:49:32 -0300

    Exercícios 3 e 4 JS

---
+++ ExercíciosJS/ex3.js
@@ -0,0 +1,4 @@
+function Soma1aN() {
+        var  numero = prompt("Digite um numero");
+    somatotal = parseInt(numero) * (parseInt(numero) + 1) / 2;
+    alert("A soma dos números inteiros de 1 a "+numero+" é de "+somatotal)}
---
+++ ExercíciosJS/ex4.js
@@ -0,0 +1,9 @@
+function npz() {
+    var numero = prompt("Digite um numero");
+
+if( numero == 2){
+	alert("O número é primo");
+}else if( numero % 2 == 0){
+	alert("O número não é primo");
+}else
+alert("O número é primo") }
